5cc153bce2baaefab8db71e00b3df31b25667b70
Thijs Creemers <thijs.creemers@gmail.com>
2017-03-20 13:57:07 +0100

    added TEMP_URL key to objectstore

--- web/ckan/docker-entrypoint.sh
+++ web/ckan/docker-entrypoint.sh
@@ -22,6 +22,10 @@ echo Building db
 cd /src/ckan
 cloudfuse -o username=catalogus,password=${CATALOGUS_OBJECTSTORE_PASSWORD},tenant=e85bcf2124fb4437b1bc6eb75dfc3abf,authurl=https://identity.stack.cloudvps.com/v2.0 /var/lib/ckan/
 
+curl -X POST -H 'X-Auth-Token: 4da79e32b89c458991b6c21d0eff7985' \
+-H 'X-Account-Meta-Temp-Url-Key: ${TEMP_URL_KEY}' \
+https://e85bcf2124fb4437b1bc6eb75dfc3abf.objectstore.eu/
+
 # init database
 paster db init -c /app/config.ini
 # rebuild solr index
