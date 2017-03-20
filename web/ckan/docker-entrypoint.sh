c2ed09b76dc539ae222c35dadaa64300585249b0
Thijs Creemers <thijs.creemers@gmail.com>
2017-03-20 14:16:30 +0100

    added Object store token

--- web/ckan/docker-entrypoint.sh
+++ web/ckan/docker-entrypoint.sh
@@ -22,8 +22,9 @@ echo Building db
 cd /src/ckan
 cloudfuse -o username=catalogus,password=${CATALOGUS_OBJECTSTORE_PASSWORD},tenant=e85bcf2124fb4437b1bc6eb75dfc3abf,authurl=https://identity.stack.cloudvps.com/v2.0 /var/lib/ckan/
 
-curl -X POST -H 'X-Auth-Token: 4da79e32b89c458991b6c21d0eff7985' \
--H 'X-Account-Meta-Temp-Url-Key: ${TEMP_URL_KEY}' \
+# Make tempurl setting
+curl -X POST -H "X-Auth-Token: ${OBJECTSTORE_TOKEN}" \
+-H "X-Account-Meta-Temp-Url-Key: ${TEMP_URL_KEY}" \
 https://e85bcf2124fb4437b1bc6eb75dfc3abf.objectstore.eu/
 
 # init database
